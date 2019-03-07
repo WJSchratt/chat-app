65588d35b853208e9a5151901477c5342b2eb0de
Walter J Schratt <john.schratt1@gmail.com>
2019-03-07 18:30:27 -0500

    finished checkpoint

--- src/components/User.js
+++ src/components/User.js
@@ -5,44 +5,43 @@ class User extends Component{
   constructor(props) {
     super(props);
 
-    this.state = {
-      userName:"",
-      k:""
-    };
+      this.state = {
+        userName:"",
+      };
 
-  }
-  componentDidMount() {
-  this.props.firebase.auth().onAuthStateChanged( user => {
-    this.props.setUser(user);
-    var user = firebase.auth().currentUser;
-    if (user) {
-      this.setState({ userName : this.props.user.displayName})
-    } else {
-      this.setState({ userName: "Guest" })
     }
-  });}
+    componentDidMount() {
+    this.props.firebase.auth().onAuthStateChanged( user => {
+      this.props.setUser(user);
+      var user = firebase.auth().currentUser;
+      if (user) {
+        this.setState({ userName : this.props.user.displayName})
+      } else {
+        this.setState({ userName: "Guest" })
+      }
+    });}
 
-signOut = () =>{
-  this.props.firebase.auth().signOut();
-}
-
-signIn = () => {
-  const provider = new this.props.firebase.auth.GoogleAuthProvider();
-  this.props.firebase.auth().signInWithPopup( provider );
-}
-
-
-  render(){
-      console.log(this.state.userName)
-    return(
-      <div>
-      <button onClick={() => this.signIn()}>Sign in</button>
-      <button onClick={() => this.signOut()}>Sign out</button>
-      <h2>{this.state.userName}</h2>
-      </div>
-    )
+  signOut = () =>{
+    this.props.firebase.auth().signOut();
   }
-}
+
+  signIn = () => {
+    const provider = new this.props.firebase.auth.GoogleAuthProvider();
+    this.props.firebase.auth().signInWithPopup( provider );
+  }
+
+
+    render(){
+        console.log(this.state.userName)
+      return(
+        <div>
+        <button onClick={() => this.signIn()}>Sign in</button>
+        <button onClick={() => this.signOut()}>Sign out</button>
+        <h2>{this.state.userName}</h2>
+        </div>
+      )
+    }
+  }
 
 
 
